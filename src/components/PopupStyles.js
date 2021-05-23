44ae10d63738e6ba24ef33ce49b06d7adec4cd01
Alisa <mme.figaro@ya.ru>
2021-05-23 16:55:19 +0300

    popup styles

---
+++ src/components/PopupStyles.js
@@ -0,0 +1,16 @@
+const popupContent = {
+  backgroundColor: "#b1cbec",
+  borderRadius: "3px",
+  padding: "3px",
+};
+
+const popupHead = {
+  fontWeight: "bold",
+  fontSize: "17px",
+};
+
+const popupText = {
+  fontSize: "15px",
+};
+
+export { popupContent, popupText, popupHead };
